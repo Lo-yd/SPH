35e53b7acbcc593c618ae14d23910fa8ccc83bf0
Jing <1344372401@qq.com>
2022-03-04 23:51:32 +0800

    update: home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,15 +2,25 @@
   <div>
     <TypeNav />
     <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
   </div>
 </template>
 
 <script>
   import ListContainer from '@/views/Home/ListContainer'
+  import Recommend from '@/views/Home/Recommend'
+  import Rank from '@/views/Home/Rank'
+  import Like from '@/views/Home/Like'
+
   export default {
     name: 'Home',
     components: {
-      ListContainer
+      ListContainer,
+      Recommend,
+      Rank,
+      Like
     }
 
   }
